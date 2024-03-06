dbed34b9c0e18cc97f6c50e8874d2a4a79e5d609
BuhleNonjojo <nonjojoo.buhle@gmail.com>
2024-03-06 17:55:18 +0200

    master

--- base_app.py
+++ base_app.py
@@ -600,12 +600,7 @@ def main():
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
 		
-       image = Image.open('resources/Blue White Creative Professional Modern Business Agency Pitch Deck Presentation Template (2).png')
-
-	 col1, col2 = st.columns([3, 3])
-	with col1:
-		st.image(image, use_column_width=True)
-	with col2:
+    
 		st.title('Lead Engine')
 	    st.title("Twitter Sentiment Classifier App")
 	
