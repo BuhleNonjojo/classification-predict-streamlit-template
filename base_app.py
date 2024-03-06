8d224ad5ee492eeb115ba908618004f2971822f8
BuhleNonjojo <nonjojoo.buhle@gmail.com>
2024-03-06 14:35:03 +0200

    master

--- base_app.py
+++ base_app.py
@@ -596,14 +596,15 @@ def main():
 	# Creates a main title and subheader on your page -
 	# these are static across all pages
 
-    image = Image.open('resources/Blue White Creative Professional Modern Business Agency Pitch Deck Presentation Template (2).png')
+image = Image.open('resources/Blue White Creative Professional Modern Business Agency Pitch Deck Presentation Template (2).png')
 
-    col1, col2 = st.columns([3, 3])
+	col1, col2 = st.columns([3, 3])
 	with col1:
 		st.image(image, use_column_width=True)
 	with col2:
 		st.title("Lead Engine")
 		st.title("Twitter Sentiment Classifier App")
+	
 
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
